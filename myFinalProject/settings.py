b0e4ec737d5fe8aa032b09dd578eb3013534a285
Richard Dalton <richarddalton@Richards-MacBook-Pro.local>
2016-11-16 12:12:13 +0000

    Added Authentication

--- myFinalProject/settings.py
+++ myFinalProject/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'accounts',
+    'django_forms_bootstrap'
 ]
 
 MIDDLEWARE = [
@@ -100,6 +102,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
+
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'accounts.backends.EmailAuth',
+)
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
